5f634f9430e44e356684e651761870fd93ae679b
[Yaswanth-B] <[yaswanthonline@gmail.com]>
2023-01-20 12:53:26 +0530

    added sidebar

--- app1.py
+++ app1.py
@@ -5,23 +5,26 @@ import tempfile
 import cv2 as cv
 
 def main():
-    st.title("Smartathon")
-    st.subheader("Detection and evaluation of the following elements on street imagery taken from a moving vehicle")
-    video_file = st.file_uploader("Upload Video",type=['mp4','mp3'])
-    if video_file is not None:
-        file_details = {"Filename":video_file.name,"FileType":video_file.type,"FileSize":video_file.size}
-        st.write(file_details)
-        tfile = tempfile.NamedTemporaryFile(delete=False) 
-        tfile.write(video_file.read())
-        vf = cv.VideoCapture(tfile.name)
-        stframe = st.empty()
+		st.title("Smartathon")
+		with st.sidebar:
+			radio_op = st.radio("MENU", ("EDA", "Model", "About"))			
+		if(radio_op == "EDA"):
+			st.subheader("Detection and evaluation of the following elements on street imagery taken from a moving vehicle")
+			video_file = st.file_uploader("Upload Video",type=['mp4','mp3'])
+			if video_file is not None:
+					file_details = {"Filename":video_file.name,"FileType":video_file.type,"FileSize":video_file.size}
+					st.write(file_details)
+					tfile = tempfile.NamedTemporaryFile(delete=False) 
+					tfile.write(video_file.read())
+					vf = cv.VideoCapture(tfile.name)
+					stframe = st.empty()
 
-        while vf.isOpened():
-            ret, frame = vf.read()
-            if not ret:
-                print("Can't receive frame (stream end?). Exiting ...")
-                break
-            # gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-            stframe.image(frame)
+					while vf.isOpened():
+							ret, frame = vf.read()
+							if not ret:
+									print("Can't receive frame (stream end?). Exiting ...")
+									break
+							# gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+							stframe.image(frame)
 if __name__ == '__main__':
     main()
